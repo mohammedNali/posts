0309c736cc8fefd7bf208fa3ea3529246924b577
mohammed <mohammdnjmali@gmail.com>
2023-11-18 07:43:43 +0300

    add files

---
+++ database/migrations/2023_11_16_182958_add_post_key_to_posts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('post_key'); // 8J2Ika91
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('post_key');
+        });
+    }
+};
